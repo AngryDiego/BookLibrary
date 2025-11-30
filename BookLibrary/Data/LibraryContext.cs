95b805c9b5435e8119b3a61b9ac29f7e559cc06b
Kekus_Gigabyte <user@WIN-G3Q8CVB0RMM>
2025-11-30 14:09:47 +0100

    init

---
+++ BookLibrary/Data/LibraryContext.cs
@@ -0,0 +1,16 @@
+﻿namespace BookLibrary.Data;
+using Microsoft.EntityFrameworkCore;
+using BookLibrary.Models;
+
+public class LibraryContext : DbContext
+{
+    // констуркор - public *className*(parametr) : base(parametr), де base - підключення наслідування
+    public LibraryContext(DbContextOptions<LibraryContext> options) : base(options)
+    {
+        
+    }
+
+    public DbSet<Book> BookShelf { get; set; }
+    
+
+}
